e1c0f0d58a69b2659b4c8da875868abeb3617d86
UdaySingh1993 <snakesolid777707@gmail.com>
2025-04-09 00:20:38 +0530

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,17 @@
+#Task 1
+Num1 = input("Please enter the first number: ")
+Num2 = input("Please enter the second number: ")
+Num1 = float(Num1)
+Num2 = float(Num2)
+
+print("Addition =",Num1 + Num2)
+print("Subtraction =",Num1-Num2)
+print("Division =",Num1/Num2)
+print("Multiplication =",Num1*Num2)
+
+#
+#Task 2
+Name1 = input("Enter your first name: ")
+Name2 = input("Enter your last name: ")
+
+print("Hello, ",Name1,Name2, "! Welcome to the Python program.")
